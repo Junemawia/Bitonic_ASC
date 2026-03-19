ddf7674f98ff925b090444271c7c237fc19b3531
june mawia <mawia9301@gmail.com>
2026-03-19 20:46:09 +0300

    implementing compswap

--- main.cpp
+++ main.cpp
@@ -4,3 +4,14 @@
 #include <climits>
 #include <cmath>
 using namespace std;
+
+long long comparisonCount = 0;
+long long swapCount = 0;
+
+void compAndSwap(vector<int>& arr, int i, int j, int dir) {
+    comparisonCount++;
+    if (dir == (arr[i] > arr[j])) {
+        swap(arr[i], arr[j]);
+        swapCount++;
+    }
+}
