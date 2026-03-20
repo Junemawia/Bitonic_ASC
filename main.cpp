5d1ee7f059384b3c38f67344743d3a7c2603f5ce
june mawia <mawia9301@gmail.com>
2026-03-20 08:56:41 +0300

    bitonic merge and sorting

--- main.cpp
+++ main.cpp
@@ -15,3 +15,14 @@ void compAndSwap(vector<int>& arr, int i, int j, int dir) {
         swapCount++;
     }
 }
+void bitonicSort(vector<int>& arr, int low, int cnt, int dir) {
+    if (cnt > 1) {
+        int k = cnt / 2;
+
+        bitonicSort(arr, low, k, 1);
+
+        bitonicSort(arr, low + k, k, 0);
+
+        bitonicMerge(arr, low, cnt, dir);
+    }
+}
